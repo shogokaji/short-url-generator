17ab941c1ffb6d3c76da3847475da4af68b8f455
Kaito1290 <sirobu9963@gmail.com>
2024-02-26 22:13:25 +0900

    POSTエンドポイントのRSpecを追加

--- spec/requests/api/v1/urls_spec.rb
+++ spec/requests/api/v1/urls_spec.rb
@@ -1,7 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe "Api::V1::Urls", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe "POST /" do
+    context 'urlが空のとき' do
+      it 'ステータス400を返す' do
+        post "/api/v1/urls", params: { url: "" }
+
+        expect(response).to have_http_status(400)
+      end
+    end
+    context 'urlが不正の場合' do
+      it 'ステータス400を返す' do
+        post "/api/v1/urls", params: { url: "htttp://localhost:3000" }
+
+        expect(response).to have_http_status(400)
+      end
+    end
   end
 end
