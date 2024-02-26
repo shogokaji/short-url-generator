1d5e6c7c9e16f1e3e3c7dc799de844a79708511f
Kaito1290 <sirobu9963@gmail.com>
2024-02-26 22:14:50 +0900

    POSTエンドポイントのバリデーション作成

--- app/controllers/api/v1/urls_controller.rb
+++ app/controllers/api/v1/urls_controller.rb
@@ -1,5 +1,23 @@
 class Api::V1::UrlsController < ApplicationController
+  before_action do
+    check_url
+  end
+  
   def create
     Url.create(original: "hoge", digest: "hoge")
+  end
+
+  private
+
+  def check_url
+    url = params[:url]
+    if url.blank? || !valid_url(url)
+      render json: {}, status: 400
+      return
+    end
+  end
+
+  def valid_url(url)
+    url.start_with?("http://") || url.start_with?("https://")
   end
 end
