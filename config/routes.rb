1a0e6eeaef9fe4766934f1b5cdbe4b522b9097a3
Kaito1290 <sirobu9963@gmail.com>
2024-03-18 22:58:11 +0900

    ルーティングを修正

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   # get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "urls#index"
   post "urls" => "urls#create"
+  get ":digest" => "urls#jump"
 end
