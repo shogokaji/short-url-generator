0354ac6dd54a556d614b5897dfc5f7dc33f85136
Kaito1290 <sirobu9963@gmail.com>
2024-04-01 22:53:52 +0900

    POSTリクエストのテストを作成

--- spec/requests/urls_spec.rb
+++ spec/requests/urls_spec.rb
@@ -17,4 +17,25 @@ RSpec.describe "Urls", type: :request do
       end
     end
   end
+
+  describe "POST /:digest" do
+    before do
+      Url.create(original: "htttp://localhost:3000", digest: "hoge")
+    end
+
+    context 'リクエストに成功したとき' do
+      it 'ステータス301を返す' do
+        get "/hoge"
+
+        expect(response).to have_http_status(301)
+      end
+    end
+    context '紐づくURLが存在しないとき' do
+      it 'ステータス404を返す' do
+        get "/huga"
+
+        expect(response).to have_http_status(404)
+      end
+    end
+  end
 end
